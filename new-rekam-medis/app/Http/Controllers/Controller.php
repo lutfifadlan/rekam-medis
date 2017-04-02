59cb0177bced108a9c90bbdd10019a037f1cc404
Mochamad Lutfi Fadlan <Mochamad Lutfi Fadlan>
2017-04-02 21:47:35 +0700

    use the real data that needed for add, and view patients

--- new-rekam-medis/app/Http/Controllers/Controller.php
+++ new-rekam-medis/app/Http/Controllers/Controller.php
@@ -12,8 +12,10 @@ use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
-use View;
 use DB;
+use Log;
+use Session;
+use View;
 
 class Controller extends BaseController
 {
@@ -60,10 +62,64 @@ class Controller extends BaseController
 
 	public function createPatient(Request $request)
 	{
-		echo $request;
+		$patientNumber = $request->input('patient_number');
+		$allergyHistory = $request->input('allergy_history');
+		$insurance = $request->input('insurance');
 		$name = $request->input('name');
+		$parents = $request->input('parents');
+		$age = $request->input('age');
+		$birthDay = $request->input('birthday');
+		$partner = $request->input('partner');
+		$gender = $request->input('gender');
+		$education = $request->input('education');
+		$religion = $request->input('religion');
+		$job = $request->input('job');
+		$marriage = $request->input('marriage');
+		$address = $request->input('address');
+		$phoneNumber = $request->input('phone_number');
+		$weight = $request->input('weight');
+		$height = $request->input('height');
+		$date = $request->input('date');
+		$anamnesa = $request->input('anamnesa');
+		$physicalExamination = $request->input('physical_examination');
+		$diagnosis = $request->input('diagnosis');
+		$act = $request->input('act');
+		$doctor = $request->input('doctor');
+
 		DB::table('patients')->insert(
-		    ['name' => $name ]
+			[
+				'patient_number' => $patientNumber,
+		    	'allergy_history' => $allergyHistory,
+		    	'insurance' => $insurance,
+				'name' => $name, 
+    			'parents' => $parents,
+				'age' => $age, 
+				'birthday' => $birthDay,
+				'partner' => $partner,
+				'gender' => $gender,
+				'education' => $education,
+				'religion' => $religion,
+				'job' => $job,
+				'marriage' => $marriage,
+				'address' => $address,
+				'phone_number' => $phoneNumber,
+				'weight' => $weight,
+				'height' => $height,
+				'date' => $date,
+				'anamnesa' => $anamnesa,
+				'physical_examination' => $physicalExamination,
+				'diagnosis' => $diagnosis,
+				'act' => $act,
+				'doctor' => $doctor
+		    ]
 		);
+
+		if (Auth::check()) {
+			Session::flash('message', 'Data berhasil disimpan!');
+			return Redirect::to('patients');
+		}
+		else {
+			return Redirect::to('login');
+		}
 	}
 }
--- new-rekam-medis/resources/views/patients.blade.php
+++ new-rekam-medis/resources/views/patients.blade.php
@@ -3,12 +3,16 @@
 @section('content')
 	<table>
 	<?php $i = 1; ?>
-	<tr><td>Nama - Umur - Tanggal Lahir - Alamat - Tanggal dibuat - Tanggal diperbaharui</td></tr>
+	<tr><td>Nomor Pasien - Riwayat Alergi - Asuransi - Nama - Orang Tua - Umur - Tanggal Lahir - Suami/Istri - Jenis Kelamin - Pendidikan - Agama - Pekerjaan - Perkawinan - Alamat - No. Telepon - Berat Badan - Tinggi Badan - Tanggal - Anamnesa - Pemeriksaan Fisik - Diagnosa - Tindakan - Dokter</td></tr>
     @foreach($patients as $patient)
-        <tr><td>{{$i}} - {{$patient->name}} - {{$patient->age}} - {{$patient->dateofbirth}} - {{$patient->address}} - {{$patient->created_at}} - {{$patient->updated_at}}</td></tr>
+        <tr><td>{{$i}} - {{$patient->patient_number}} - {{$patient->allergy_history}} - {{$patient->insurance}} - {{$patient->name}} - {{$patient->parents}} - {{$patient->age}} - {{$patient->birthday}} - {{$patient->partner}} - {{$patient->gender}} - {{$patient->education}} - {{$patient->religion}} - {{$patient->job}} - {{$patient->marriage}} - {{$patient->address}} - {{$patient->phone_number}} - {{$patient->weight}} - {{$patient->height}} - {{$patient->date}} - {{$patient->anamnesa}} - {{$patient->physical_examination}} - {{$patient->diagnosis}} - {{$patient->act}} - {{$patient->doctor}}</td></tr>
         <?php $i++; ?>
     @endforeach
 	</table>
 	<a href="{{ URL::to('logout') }}">Logout</a>
 	<a href="{{ URL::to('patients_form') }}"> Add Patient </a>
 @stop
+
+@if(Session::has('message'))
+<p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+@endif
