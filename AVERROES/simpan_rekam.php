54d88f3179953a96486360b3326539f6b86701ab
Damon Prasetyo Arso <damonarso@gmail.com>
2017-05-14 23:44:09 +0700

    variabel dibagusin
    biar variabel gak kayak ngasal

--- AVERROES/simpan_rekam.php
+++ AVERROES/simpan_rekam.php
@@ -4,29 +4,27 @@ $alamat = $_REQUEST['alamat'];
 $nomor = $_REQUEST['nomor'];
 $alergi = $_REQUEST['alergi'];
 $asuransi =  $_REQUEST['asuransi']; 
-$ortu = $_REQUEST['ortu'];
+$orang_tua = $_REQUEST['orang_tua'];
 $umur = $_REQUEST['umur'];
-$TL = $_REQUEST['TL'];
+$Tanggal_Lahir = $_REQUEST['Tanggal_Lahir'];
 $pasangan =  $_REQUEST['pasangan']; 
-$kelamin = $_REQUEST['kelamin'];
-$sangdik = $_REQUEST['sangdik'];
+$jenis_kelamin = $_REQUEST['jenis_kelamin'];
+$Pendidikan_Pasangan = $_REQUEST['Pendidikan_Pasangan'];
 $agama = $_REQUEST['agama'];
-$sangker =  $_REQUEST['sangker']; 
-$kawin = $_REQUEST['kawin'];
-$no_hp = $_REQUEST['no_hp'];
-$produk = $_REQUEST['produk'];
-$didik = $_REQUEST['didik'];
-$telp =  $_REQUEST['telp']; 
-$kerja = $_REQUEST['kerja'];
-$bb = $_REQUEST['bb'];
-$tb = $_REQUEST['tb'];
+$Pekerjaan_Pasangan =  $_REQUEST['Pekerjaan_Pasangan']; 
+$perkawinan = $_REQUEST['perkawinan'];
+$pendidikan = $_REQUEST['pendidikan'];
+$nomor_telpon =  $_REQUEST['nomor_telpon']; 
+$pekerjaan = $_REQUEST['pekerjaan'];
+$berat_badan = $_REQUEST['berat_badan'];
+$tinggi_badan = $_REQUEST['tinggi_badan'];
 
 // echo $produk[2];
 // return;
 $servername = "localhost";
 $username = "root"; // "root" "kainkady_root"
 $password = ""; // "" "494032"
-$dbname = ""; //"kainka" "kainkady_kainka"
+$dbname = "Rekam_Medis"; //"kainka" "kainkady_kainka"
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -35,12 +33,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO `order` (nama, alamat, no_hp)
-VALUES ('$nama', '$alamat', '$no_hp')";
+$sql = "INSERT INTO `rekam` (nomor, alergi, asuransi, nama, orang_tua, Tanggal_Lahir, pasangan, jenis_kelamin, Pendidikan_Pasangan, agama, Pekerjaan_Pasangan, perkawinan, alamat, pendidikan, nomor_telpon, pekerjaan, berat_badan, tinggi_badan)
+VALUES ('$nomor', '$alergi', '$asuransi', '$nama', '$orang_tua', '$Tanggal_Lahir', '$pasangan', '$jenis_kelamin', '$Pendidikan_Pasangan', '$agama', '$Pekerjaan_Pasangan', '$perkawinan', '$alamat', '$pendidikan', '$nomor_telpon', '$pekerjaan', '$berat_badan', '$tinggi_badan')";
 
 if ($conn->query($sql) === TRUE) {
-    // echo "New record " . $conn->insert_id . " created successfully";
-   	$id_order = $conn->insert_id;
+    echo "New record " . $conn->insert_id . " created successfully";
+   	/**$id_order = $conn->insert_id;
     foreach ($produk as $id_produk => $jumlah) {
     	if ($jumlah < 1) {
     		continue;
@@ -48,7 +46,7 @@ if ($conn->query($sql) === TRUE) {
     	$sql = "INSERT INTO `order_produk` (id_order, id_produk, jumlah)
 			VALUES ('$id_order', '$id_produk', '$jumlah')";
 		$conn->query($sql);
-    }
+    }**/
 
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
